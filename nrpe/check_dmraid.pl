f0d4e28a22a719e79355efca24c4c9c3dad09057
Joel Bastos <kintoandar@gmail.com>
2011-11-14 12:09:40 +0000

    edit nrpe script

--- nrpe/check_dmraid.pl
+++ nrpe/check_dmraid.pl
@@ -9,7 +9,7 @@ chomp ($raid_status[0]);
 chomp ($raid_status[1]);
 
 if ($raid_status[0] eq "status : ok" && $raid_status[1] eq "status : ok"){
-        print "OK : RAID Up and Running \n";
+        print "OK : RAID consistent \n";
         exit 0;
 }else{
         print "CRITICAL : RAID problem detected \n";
