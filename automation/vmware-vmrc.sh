7885c3b177dfa7075affd432fb6af3a79416ba61
Joel Bastos <kintoandar@gmail.com>
2011-11-14 12:34:13 +0000

    add vmware console fix script

---
+++ automation/vmware-vmrc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+############################################################################################
+# kintoandar
+# check http://kintoandar.blogspot.com/2011/06/gnome-3-vmware-console-with-vmware-vmrc.html
+# for more info on this script
+############################################################################################
+
+# Please define this variable with the path to the plugins folder
+PATH_TO_VMRC="/home/user/vmware-vmrc-plugin"
+
+export VMWARE_USE_SHIPPED_GTK=yes
+cd $PATH_TO_VMRC
+./vmware-vmrc > /dev/null 2>&1 &
+cd - > /dev/null 2>&1
+
