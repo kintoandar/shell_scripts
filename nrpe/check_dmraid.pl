786b1e4b9f08d3a04d90906607119d31ac403786
Joel Bastos <kintoandar@gmail.com>
2011-11-14 12:07:23 +0000

    add nrpe script

---
+++ nrpe/check_dmraid.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $raid_status="";
+my @raid_status=`/sbin/dmraid -s | grep status `;
+
+chomp ($raid_status[0]);
+chomp ($raid_status[1]);
+
+if ($raid_status[0] eq "status : ok" && $raid_status[1] eq "status : ok"){
+        print "OK : RAID Up and Running \n";
+        exit 0;
+}else{
+        print "CRITICAL : RAID problem detected \n";
+        exit 2;
+
+}
+
+print "Unkown error: It shouldn't get here.";
+exit 3;
